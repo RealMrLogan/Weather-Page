f40f7f12f87687ba4e8a4af668546e18602e725f
loganes1 <sau13007@byui.edu>
2018-11-17 10:14:32 -0700

    added the JS to changes the pictures based on weather condition

--- homework/weather/js/functions.js
+++ homework/weather/js/functions.js
@@ -22,55 +22,115 @@ function buildWC(speed, temp) {
    feelTemp.innerHTML = wc;
 }
 
-const temp = 31;
-const speed = 5;
-buildWC(speed, temp);
+windDial("SE")
 
-const direction = "NNE"; //Set your own value
-windDial(direction);
-
-// Wind Dial Function
+// Rotate the wind Dial
 function windDial(direction) {
    // Get the container
    const dial = document.getElementById("dial");
-   
+   const windDirection = document.getElementById("wind-direction");
+
    // Determine the dial class
    switch (direction) {
       case "North":
       case "N":
          dial.setAttribute("class", "n"); //"n" is the CSS rule selector
+         windDirection.innerHTML = "N";
          break;
       case "NE":
       case "NNE":
       case "ENE":
          dial.setAttribute("class", "ne");
+         windDirection.innerHTML = "NE";
          break;
       case "NW":
       case "NNW":
       case "WNW":
          dial.setAttribute("class", "nw");
+         windDirection.innerHTML = "NW";
          break;
       case "South":
       case "S":
          dial.setAttribute("class", "s");
+         windDirection.innerHTML = "S";
          break;
       case "SE":
       case "SSE":
       case "ESE":
          dial.setAttribute("class", "se");
+         windDirection.innerHTML = "SE";
          break;
       case "SW":
       case "SSW":
       case "WSW":
          dial.setAttribute("class", "sw");
+         windDirection.innerHTML = "SW";
          break;
       case "East":
       case "E":
          dial.setAttribute("class", "e");
+         windDirection.innerHTML = "E";
          break;
       case "West":
       case "W":
          dial.setAttribute("class", "w");
+         windDirection.innerHTML = "W";
          break;
    }
 }
+
+//Get the weather condition
+function getCondition(currentCondition) {
+   let weatherCondition = "";
+
+   if (currentCondition.includes("sunny") || currentCondition.includes("clear")) {
+      weatherCondition = "clear";
+   } else if (currentCondition.includes("cloud") || currentCondition.includes("overcast")) {
+      weatherCondition = "clouds";
+   } else if (currentCondition.includes("rain") || currentCondition.includes("wet")) {
+      weatherCondition = "rain";
+   } else if (currentCondition.includes("fog")) {
+      weatherCondition = "fog";
+   } else if (currentCondition.includes("snow")) {
+      weatherCondition = "snow";
+   } else {
+      console.log(`"${currentCondition}" does not match a weather condition.`);
+   }
+
+   console.log(`The current condition is ${weatherCondition}.`);
+
+   return weatherCondition;
+}
+
+// change the weather picture
+function changeSummaryImage(weatherCondition) {
+   const widgets = document.getElementsByClassName("widgets")[0];
+
+   switch (weatherCondition) {
+      case "clear":
+      console.log(`Setting the image to ${weatherCondition}.`);
+         widgets.setAttribute("class", "widgets clear");
+         break;
+      case "clouds":
+      console.log(`Setting the image to ${weatherCondition}.`);
+         widgets.setAttribute("class", "widgets clouds");
+         break;
+      case "fog":
+      console.log(`Setting the image to ${weatherCondition}.`);
+         widgets.setAttribute("class", "widgets fog");
+         break;
+      case "rain":
+      console.log(`Setting the image to ${weatherCondition}.`);
+         widgets.setAttribute("class", "widgets rain");
+         break;
+      case "snow":
+      console.log(`Setting the image to ${weatherCondition}.`);
+         widgets.setAttribute("class", "widgets snow");
+         break;
+      default:
+      console.log(`${weatherCondition} did not match any cases.`);
+         break;
+   }
+}
+
+changeSummaryImage(getCondition("cloudy"));
